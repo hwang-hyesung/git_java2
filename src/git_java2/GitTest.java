36539b1737e7ca7ef6df5aa9cae52f483cc2903e
hwang-hyesung <119783622+hwang-hyesung@users.noreply.github.com>
2025-05-07 17:19:57 +0900

    second commit

--- src/git_java2/GitTest.java
+++ src/git_java2/GitTest.java
@@ -3,7 +3,8 @@ package git_java2;
 public class GitTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("qwerwetytuioyu");
+		System.out.println("qwerwetytuioyu");
 		System.out.println("qwerwetytuioyu");
 	}
 
